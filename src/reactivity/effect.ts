ad8242e5576cef101256a323bd66796d774ac44c
yiming.cui <cuiyiming@thinkenergy.net.cn>
2022-06-13 19:06:22 +0800

    feat: effect

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,40 @@
+
+class ReactiveEffect {
+  private _fn: any
+
+  constructor(fn) {
+    this._fn = fn
+  }
+  run() {
+    activeEffect = this
+    this._fn()
+  }
+}
+let activeEffect
+const targetMap = new Map()
+export function track(target, key) {
+  // target -> key -> dep
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    targetMap.set(target, depsMap = new Map())
+  }
+  let dep = depsMap.get(key)
+  if (!dep) {
+    depsMap.set(key, dep = new Set())
+  }
+
+  dep.add(activeEffect)
+}
+
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target)
+  let dep = depsMap.get(key)
+  for (const effect of dep) {
+    effect.run()
+  }
+}
+
+export function effect(fn) {
+  const _effect = new ReactiveEffect(fn)
+  _effect.run()
+}
