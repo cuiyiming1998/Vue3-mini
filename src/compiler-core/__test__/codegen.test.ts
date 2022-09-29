6c9f16218b7127155e39ec8255e01fe4c2dd65ee
cuiyiming1998 <1269567664@qq.com>
2022-09-29 17:26:52 +0800

    ✨ feat: 实现代码生成string类型

---
+++ src/compiler-core/__test__/codegen.test.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect, vi } from 'vitest'
+import { generate } from '../src/codegen';
+import { baseParse } from "../src/parse";
+import { transform } from "../src/transform";
+
+
+describe("codegen", () => {
+  it("string", () => {
+    const ast = baseParse("hi");
+    transform(ast);
+    const { code } = generate(ast);
+    expect(code).toMatchSnapshot();
+  });
+})
---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,46 @@
+import { NodeTypes } from './ast'
+
+export function transform(root, options = {}) {
+	// 1. 遍历
+	// 深度优先搜索ast树
+	const context = createTransformContext(root, options)
+	traverseNode(root, context)
+	// 2. 修改
+  // root.codegenNode
+  createRootCodegen(root)
+}
+
+function traverseNode(node: any, context) {
+	console.log(node)
+
+	// 由外部传入的transforms来驱动
+	const nodeTransforms = context.nodeTransforms
+	for (let i = 0; i < nodeTransforms.length; i++) {
+		const transform = nodeTransforms[i]
+		transform(node)
+	}
+	traverseChildren(node, context)
+}
+function traverseChildren(node: any, context: any) {
+	const children = node.children
+
+	if (children) {
+		for (let i = 0; i < children.length; i++) {
+			const node = children[i]
+			traverseNode(node, context)
+		}
+	}
+}
+
+function createTransformContext(root: any, options: any) {
+	const context = {
+		root,
+		nodeTransforms: options.nodeTransforms || []
+	}
+
+	return context
+}
+function createRootCodegen(root: any) {
+  root.codegenNode = root.children[0]
+}
+
